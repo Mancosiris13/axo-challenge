6691e97a20c68bcf8e5a77acec8e47e76fa770f2
omacias <vguzman@grupoaxo.com>
2024-11-19 12:22:53 -0600

    Testing Osiris Credentials correctly - 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Footer from "./components/Footer/Footer";
-import NavBar from "./components/NavBar/NavBar";
-import Products from "./components/Products/Products";
+// import Footer from "./components/Footer/Footer";
+import NavBar from './components/NavBar/NavBar';
+import Products from './components/Products/Products';
 
 function App() {
   return (
     <>
       <NavBar />
       <Products />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
